9d392c74c39029d23fedd652e9ab898748775bb6
Samee Rauf <samee.rauf@pmg.com>
2024-12-30 16:47:28 -0800

    added samosa image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import samosaImage from './samosa.webp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Hello</h1>
-      </header>
+        <header className="App-header">
+            <h1> Samosa Selector</h1>
+            <img src={samosaImage} alt="Samosa" style={{width: '200px', marginBottom: '20px'}}/>
+
+        </header>
     </div>
   );
 }
