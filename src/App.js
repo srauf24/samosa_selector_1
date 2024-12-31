65fe2159ec67101b7d6cfd8c77d196f14c4504c0
Samee Rauf <samee.rauf@pmg.com>
2024-12-31 07:56:00 -0800

    added double stuff party and feast options

--- src/App.js
+++ src/App.js
@@ -7,10 +7,25 @@ function App() {
     const handleSingleClick = () => {
         setCount(count + 1);
     };
-    const handleDoubleClick = () => {
+    const buyDoubleStuffed = () => {
         if (count >= 10)
         {
-            setCount(count + 10);
+            setCount((prevCount) => (prevCount * 2) - 10);
+
+        }
+    };
+    const buyPartyPack = () => {
+        if (count >= 100)
+        {
+                    setCount((prevCount) => (prevCount * 100) - 100);
+
+        }
+    };
+    const buyFullFeastPack = () => {
+        if (count >= 1000)
+        {
+                    setCount((prevCount) => (prevCount * 1000) - 1000);
+
         }
     };
 
@@ -26,17 +41,31 @@ function App() {
             <h1> Count: {count}</h1>
 
 
-        <div className= "Container">
-            <div className="Upgrade">
-                <h3> Double Stuffed</h3>
-                <p> Double stuffed ground beef</p>
-                <button className = "Button" onClick={handleDoubleClick}>
-                    10 Samosas
-                </button>
+            <div className="Container">
+                <div className="Upgrade">
+                    <h3> Double Stuffed</h3>
+                    <p> Double stuffed ground beef</p>
+                    <button className="Button" onClick={buyDoubleStuffed}>
+                        10 Samosas
+                    </button>
+                </div>
+                <div className="Upgrade">
+                    <h3> Party Pack</h3>
+                    <p> Grab some samosas for your family</p>
+                    <button className="Button" onClick={buyPartyPack}>
+                        100 Samosas
+                    </button>
+                </div>
+                <div className="Upgrade">
+                    <h3> Full Feast</h3>
+                    <p> Grab some samosas for an event/party</p>
+                    <button className="Button" onClick={buyFullFeastPack}>
+                        1000 Samosas
+                    </button>
+                </div>
+
+
             </div>
-
-
-        </div>
         </header>
     </div>
   );
