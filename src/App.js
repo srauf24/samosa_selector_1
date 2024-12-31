8ddb0404d232ffa36b32baa2d01a7611b01da18b
Samee Rauf <samee.rauf@pmg.com>
2024-12-30 17:01:26 -0800

    added samosa clicker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import React, { useState } from 'react';
 import samosaImage from './samosa.webp';
 
 function App() {
+    const [count, setCount] = useState(0);
+    const handleSingleClick = () => {
+        setCount(count + 1);
+    };
+
   return (
     <div className="App">
         <header className="App-header">
             <h1> Samosa Selector</h1>
-            <img src={samosaImage} alt="Samosa" style={{width: '200px', marginBottom: '20px'}}/>
+            <img src={samosaImage}
+                 alt="Samosa"
+                 style={{width: '200px', marginBottom: '20px'}}
+                 onClick={handleSingleClick}
+
+            />
+            <p>Count: {count}</p>
 
         </header>
     </div>
